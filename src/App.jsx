0b166bde0e3f784e5774d2ead351a7524e91c60c
SharkSpidy <josephshibuwork@gmail.com>
2025-12-09 03:34:19 +0530

    APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
 import Guidelines from './pages/Guidelines'
 import Register from './pages/Register'
@@ -7,17 +7,15 @@ import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/guidelines" element={<Guidelines />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/payment" element={<Payment />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/guidelines" element={<Guidelines />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/payment" element={<Payment />} />
+      </Routes>
+    </div>
   )
 }
 
